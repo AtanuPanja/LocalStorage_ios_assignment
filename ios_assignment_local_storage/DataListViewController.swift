63a1e7982f0b8c47e4cabc9f05ef0f68deeaccae
Atanu Panja <74297632+AtanuPanja@users.noreply.github.com>
2024-02-15 18:06:33 +0530

    Adding code files for the Local Storage assignment

---
+++ ios_assignment_local_storage/DataListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataListViewController.swift
+//  ios_assignment_local_storage
+//
+//  Created by promact on 15/02/24.
+//
+
+import UIKit
+
+class DataListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
